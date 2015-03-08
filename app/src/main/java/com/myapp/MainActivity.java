edc602eb656a6b88687e7cbdb8bfa1429041c344
Zac <dantepsychedelico@gmail.com>
2015-03-09 07:54:41 +0800

    connect to server and get id done

--- app/src/main/java/com/myapp/MainActivity.java
+++ app/src/main/java/com/myapp/MainActivity.java
@@ -4,16 +4,37 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+//import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+import org.json.JSONException;
 
+import java.io.IOException;
+
+public class MainActivity extends Activity {
+    static public int uid = 0;
+//    private Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences mPrefs = getSharedPreferences("label", 0);
+        uid = mPrefs.getInt("uid", 0);
+
+        clientSocket cs = new clientSocket();
+        cs.setPrefs(mPrefs);
+        Thread cThread = new Thread(cs);
+        if (uid==0) {
+            cs.setReq("method", "new");
+        } else {
+            cs.setReq("method", "online");
+            cs.setReq("id", uid);
+        }
+        cThread.start();
 
         FragmentManager fm = getFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.activity_main);
